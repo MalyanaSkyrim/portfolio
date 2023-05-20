46adcf0edecd5423f2b048effe69e577c8326ee2
MALYANA Mohamed <malyanamohamed1@gmail.com>
2023-05-20 02:24:55 +0100

    setup tailwind, add utils and create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from "./components";
 
-import './App.css'
-
- const App = () => {
-
-
+const App = () => {
   return (
-   <div>Developer portfolio</div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
